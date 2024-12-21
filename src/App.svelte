5b817887c32dad37bb14b1861834ba2e46ddc124
xnought <dbertucci6@gatech.edu>
2024-12-21 15:51:21 -0800

    template v2

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 </script>
 
-<main></main>
+<article>
+	<a href="https://donnybertucci.com">donnybertucci.com</a>
+	<h1>You Don't Understand</h1>
+
+	<section>
+		<ul>
+			<li><a href="/">Numbers</a></li>
+			<li>
+				<a href="/">Trigonometry</a>
+			</li>
+		</ul>
+	</section>
+</article>
 
 <style>
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import { mount } from "svelte";
+import "./app.css";
+import "./tufte-css/tufte.css";
+import App from "./App.svelte";
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
-})
+	target: document.getElementById("app")!,
+});
 
-export default app
+export default app;
